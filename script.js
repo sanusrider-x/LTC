9b28eccd9201c744316c46f03367a5c2fc3526b5
sanusrider-x <sanusrider88@gmail.com>
2025-12-29 20:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+const tg = window.Telegram.WebApp;
+tg.ready();
+
+const user = tg.initDataUnsafe?.user;
+const avatar = document.getElementById("userAvatar");
+
+if (user && user.first_name) {
+  const name = user.first_name;
+
+  avatar.src =
+    "https://ui-avatars.com/api/?" +
+    "name=" + encodeURIComponent(name) +
+    "&background=22c55e" +
+    "&color=ffffff" +
+    "&rounded=true" +
+    "&size=128";
+} else {
+  avatar.src = "user.png"; // fallback image
+}
